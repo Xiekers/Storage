68065f251dedc283164b1d7c7b922c8e3cf11ded
imxieke <imxieke@qq.com>
2016-08-05 05:10:30 -0400

    update from canada vps

---
+++ Shell/samh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sed -i 's/http:\/\/amh.sh\//http:\/\/amh5.1.ckm.pw\//g' /usr/local/amh-5.1/web/index.php;
+sed -i 's/http:\/\/amh.sh\//http:\/\/amh5.1.ckm.pw\//g' /usr/local/amh-5.1/web/View/js/index.js;
+sed -i 's/http:\/\/amh.sh\//http:\/\/amh5.1.ckm.pw\//g' /usr/local/amh-5.1/web/View/config_index.php;
+sed -i 's/http:\/\/amh.sh\//http:\/\/amh5.1.ckm.pw\//g' /root/amh/conf/amh-base;
+echo "破解进程已经全部 http://amh5.1.ckm.pw/";
+echo "开发者并不容易,请尊重正版授权";
+echo "我们已经购买超长时间的正版授权";
+echo "如果您的预算不够购买正版授权请在http://ckm.pw/捐赠我们";
+echo "捐赠全部金额将会用于正版授权购买和维持这项服务";
+echo "";
+echo "此服务请只用于技术交流和免费空间业务,由此服务发生的一切问题不予承担任何责任";
+
